0c57cdc2c4df24ec0026803859d2d7ca8e9430d9
Fabricio Silva Da Rocha <fabricio.srocha@aec.com.br>
2025-02-10 08:24:00 -0300

    0003 - Created Products

---
+++ src/Ambev.DeveloperEvaluation.Application/Products/IProductQueries.cs
@@ -0,0 +1,8 @@
+﻿using Ambev.DeveloperEvaluation.Common.Utils;
+
+namespace Ambev.DeveloperEvaluation.Application.Products;
+
+public interface IProductQueries
+{
+    Task<PaginatedList<ListProductsItemResponseDTO>> ListProductsAsync(ListProductsFilterDTO filter);
+}
