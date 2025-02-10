f69344f4a2f7a0395f58547315c270e6fe68d79c
Fabricio Silva Da Rocha <fabricio.srocha@aec.com.br>
2025-02-10 08:08:28 -0300

    0002 - Created Sales Item

---
+++ src/Ambev.DeveloperEvaluation.Domain/Entities/SaleItem.cs
@@ -0,0 +1,56 @@
+﻿namespace Ambev.DeveloperEvaluation.Domain.Entities;
+
+public class SaleItem
+{
+    /// <summary>
+    /// Gets the identifier of sale associated with the sale item
+    /// </summary>
+    public Guid SaleId { get; set; }
+
+    /// <summary>
+    /// Gets the identifier of product associated with the sale item
+    /// </summary>
+    public Guid ProductId { get; set; }
+
+    /// <summary>
+    /// Gets quantity of products
+    /// </summary>
+    public int Quantity { get; set; }
+
+    /// <summary>
+    ///Gets unit price of product
+    /// </summary>
+    public decimal UnitPrice { get; set; }
+
+    /// <summary>
+    /// Property mapping of EF Core.<br /> 
+    /// </summary
+    public Sale Sale { get; set; }
+
+    /// <summary>
+    /// Property mapping of EF Core.<br /> 
+    /// </summary
+    public Product Product { get; set; }
+
+    public decimal CalculateValue()
+    {
+        return Quantity * UnitPrice;
+    }
+
+    public bool HasUnits()
+    {
+        return Quantity > 0;
+    }
+
+    public void AddUnits(int units)
+    {
+        Quantity += units;
+    }
+
+    public void UpdateUnits(int units)
+    {
+        Quantity = units;
+    }
+
+
+}
---
+++ src/Ambev.DeveloperEvaluation.ORM/Mapping/SaleItemConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Ambev.DeveloperEvaluation.ORM.Mapping;
+
+public class SaleItemConfiguration : IEntityTypeConfiguration<SaleItem>
+{
+    public void Configure(EntityTypeBuilder<SaleItem> builder)
+    {
+        builder.ToTable("SaleItems");
+
+        builder.HasKey(si => new { si.SaleId, si.ProductId });
+
+        builder.HasOne(si => si.Sale)
+            .WithMany(s => s.SaleItems)
+            .HasForeignKey(si => si.SaleId);
+
+        builder.HasOne(si => si.Product)
+            .WithMany(p => p.SaleItems)
+            .HasForeignKey(si => si.ProductId);
+
+    }
+}
